b29566c736d3a6ec35498e22d4afd22b70d7550e
刘----仨 <13740312+liu---san@user.noreply.gitee.com>
2025-07-01 13:20:37 +0800

    给src配置别名

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     vue(),
     AutoImport({
