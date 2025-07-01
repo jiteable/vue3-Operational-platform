761461fbeee18c1553ded915adeff37c50d32e1e
刘----仨 <13740312+liu---san@user.noreply.gitee.com>
2025-07-01 11:44:38 +0800

    element-plus的集成

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -13,6 +13,15 @@ import HelloWorld from "./components/HelloWorld.vue";
       </a>
     </div>
     <HelloWorld msg="Vite + Vue" />
+
+    <div class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
   </div>
 </template>
 
--- project/src/main.ts
+++ project/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus, {
+  locale: zhCn
+}).mount('#app')
+
+
